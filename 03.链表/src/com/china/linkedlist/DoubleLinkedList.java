8554bdb79f49848eac13823f3b22727c41600f98
wuchao <18896820691@163.com>
2020-06-10 15:33:32 +0800

    双向链表的案例实现及测试

---
+++ 03.链表/src/com/china/linkedlist/DoubleLinkedList.java
@@ -0,0 +1,200 @@
+package com.china.linkedlist;
+
+/**
+ * @Author: china wu
+ * @Description: 双向链表实现案例
+ * @Date: 2020/6/10 12:37
+ */
+public class DoubleLinkedList {
+
+    /**
+     * 初始化头结点
+     */
+    private HeroNode head = new HeroNode(0, "", "");
+
+    /**
+     * 从链表最后添加结点，不考虑no编号
+     *
+     * @param node
+     */
+    public void add(HeroNode node) {
+        // 定义一个辅助结点用于遍历链表
+        HeroNode temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        // 将最后一个结点的后指针指向新结点，将新结点的前指针指向最后一个结点
+        temp.next = node;
+        node.pre = temp;
+    }
+
+    /**
+     * 根据编号顺序添加结点
+     *
+     * @param node
+     */
+    public void addByNoOrder(HeroNode node) {
+        // 定义一个辅助结点
+        HeroNode temp = head;
+        boolean flag = false;
+        while (true) {
+            // temp已经在链表的最后
+            if (temp.next == null) {
+                break;
+            }
+            // temp下一个结点比添加结点的no大，则说明temp的no小于添加结点的no
+            if (temp.next.no > node.no) {
+                break;
+            }
+            // 该结点已经存在不能添加
+            if (temp.next.no == node.no) {
+                flag = true;
+                break;
+            }
+            temp = temp.next;
+        }
+        if (flag) {
+            System.out.printf("该结点的编号%d已经存在，不能添加\n", node.no);
+        } else {
+            if (temp.next != null) {
+                node.next = temp.next;
+                temp.next.pre = node;
+            }
+            temp.next = node;
+            node.pre = temp;
+        }
+    }
+
+    /**
+     * 根据编号修改结点属性
+     *
+     * @param node 新结点
+     */
+    public void update(HeroNode node) {
+        HeroNode temp = head.next;
+        boolean flag = false;
+        while (true) {
+            if (temp == null) {
+                break;
+            }
+            if (temp.no == node.no) {
+                flag = true;
+                break;
+            }
+            temp = temp.next;
+        }
+        if (flag) {
+            temp.name = node.name;
+            temp.nickname = node.nickname;
+        } else {
+            System.out.printf("编号为%d的结点不存在，无法更新\n", node.no);
+        }
+    }
+
+    /**
+     * 根据编号来删除结点(思路:直接找到待删除的结点，修改前后指针来自我删除)
+     *
+     * @param no
+     */
+    public void deleteByNo(int no) {
+        if (head.next == null) {
+            System.out.println("链表为空，无法删除");
+            return;
+        }
+        HeroNode temp = head.next;
+        boolean flag = false;
+        while (true) {
+            if (temp == null) {
+                break;
+            }
+            if (temp.no == no) {
+                flag = true;
+                break;
+            }
+            temp = temp.next;
+        }
+        if (flag) {
+            temp.pre.next = temp.next;
+            // 要判断删除的是否为尾结点，否则会报空指针
+            if (temp.next != null) {
+                temp.next.pre = temp.pre;
+            }
+        } else {
+            System.out.printf("编号为%d的结点不存在，无法删除\n", no);
+        }
+    }
+
+
+    /**
+     * 展示链表所有数据
+     */
+    public void list() {
+        HeroNode temp = head.next;
+        if (temp == null) {
+            System.out.println("链表为空");
+            return;
+        }
+        while (temp != null) {
+            System.out.println(temp);
+            temp = temp.next;
+        }
+    }
+
+    /**
+     * 获取节点对象
+     *
+     * @param no
+     * @param name
+     * @param nickname
+     * @return
+     */
+    public HeroNode getNodeInstance(int no, String name, String nickname) {
+        return new HeroNode(no, name, nickname);
+    }
+
+    /**
+     * 双向链表结点结构
+     */
+    public class HeroNode {
+
+        /**
+         * 编号
+         */
+        private int no;
+
+        /**
+         * 姓名
+         */
+        private String name;
+
+        /**
+         * 昵称
+         */
+        private String nickname;
+
+        /**
+         * 前一个结点指针域(默认为null)
+         */
+        private HeroNode pre;
+
+        /**
+         * 下一个结点指针域(默认为null)
+         */
+        private HeroNode next;
+
+        public HeroNode(int no, String name, String nickname) {
+            this.no = no;
+            this.name = name;
+            this.nickname = nickname;
+        }
+
+        @Override
+        public String toString() {
+            return "HeroNode{" +
+                    "no=" + no +
+                    ", name='" + name + '\'' +
+                    ", nickname='" + nickname + '\'' +
+                    '}';
+        }
+    }
+}
---
+++ 03.链表/src/com/china/test/DoubleLinkedListTest.java
@@ -0,0 +1,61 @@
+package com.china.test;
+
+import com.china.linkedlist.DoubleLinkedList;
+import org.junit.Test;
+
+/**
+ * @Author: china wu
+ * @Description: 双向链表测试
+ * @Date: 2020/6/10 13:50
+ */
+public class DoubleLinkedListTest {
+
+    private DoubleLinkedList doubleLinkedList = new DoubleLinkedList();
+
+    private DoubleLinkedList.HeroNode node1 = doubleLinkedList.getNodeInstance(1, "宋江", "及时雨");
+    private DoubleLinkedList.HeroNode node2 = doubleLinkedList.getNodeInstance(2, "卢俊义", "玉麒麟");
+    private DoubleLinkedList.HeroNode node3 = doubleLinkedList.getNodeInstance(3, "吴用", "智多星");
+    private DoubleLinkedList.HeroNode node4 = doubleLinkedList.getNodeInstance(4, "林冲", "豹子头");
+
+    @Test
+    public void testAdd() {
+        doubleLinkedList.add(node1);
+        doubleLinkedList.add(node3);
+        doubleLinkedList.add(node4);
+        doubleLinkedList.add(node2);
+
+        doubleLinkedList.list();
+    }
+
+    @Test
+    public void testAddByNoOrder() {
+        doubleLinkedList.addByNoOrder(node2);
+        doubleLinkedList.addByNoOrder(node1);
+        doubleLinkedList.addByNoOrder(node4);
+        doubleLinkedList.addByNoOrder(node3);
+
+        doubleLinkedList.list();
+    }
+
+    @Test
+    public void testUpdate() {
+        testAdd();
+        System.out.println("--------------");
+        DoubleLinkedList.HeroNode node = doubleLinkedList.getNodeInstance(2, "小卢", "玉麒麟");
+        doubleLinkedList.update(node);
+        doubleLinkedList.list();
+    }
+
+    @Test
+    public void testDelete() {
+        testAdd();
+        System.out.println("--------------");
+        doubleLinkedList.deleteByNo(2);
+        doubleLinkedList.deleteByNo(4);
+        doubleLinkedList.deleteByNo(1);
+        doubleLinkedList.deleteByNo(3);
+
+        doubleLinkedList.list();
+    }
+
+}
