c7a09d7de0449e68e41818720210239692a22bf7
wuchao <18896820691@163.com>
2020-06-10 14:04:30 +0800

    单链表的案例实现及测试

--- 03.链表/src/com/china/linkedlist/SingleLinkedList.java
+++ 03.链表/src/com/china/linkedlist/SingleLinkedList.java
@@ -13,40 +13,40 @@ public class SingleLinkedList {
     private HeroNode head = new HeroNode(0, "", "");
 
     /**
-     * 从链表最后添加节点，不考虑no编号
+     * 从链表最后添加结点，不考虑no编号
      *
      * @param heroNode
      */
     public void add(HeroNode heroNode) {
-        // 定义一个辅助节点用于遍历链表
+        // 定义一个辅助结点用于遍历链表
         HeroNode temp = head;
         while (temp.next != null) {
             temp = temp.next;
         }
-        //
+        // 将最后一个结点的next指向新结点
         temp.next = heroNode;
     }
 
     /**
-     * 根据编号顺序添加节点
+     * 根据编号顺序添加结点
      *
      * @param heroNode
      */
     public void addByNoOrder(HeroNode heroNode) {
-        // 定义一个辅助节点
+        // 定义一个辅助结点
         HeroNode temp = head;
-        // flag标识是否找到添加节点所在的位置
+        // flag标识是否找到添加结点所在的位置
         boolean flag = false;
         while (true) {
             // temp已经在链表的最后
             if (temp.next == null) {
                 break;
             }
-            // 如果temp下一个节点的no大于添加节点的no，则添加节点的位置就是temp之后
+            // 如果temp下一个结点的no大于添加结点的no，则添加结点的位置就是temp之后
             if (temp.next.no > heroNode.no) {
                 break;
             }
-            // 该节点已经存在不能添加
+            // 该结点已经存在不能添加
             if (temp.next.no == heroNode.no) {
                 flag = true;
                 break;
@@ -54,7 +54,7 @@ public class SingleLinkedList {
             temp = temp.next;
         }
         if (flag) {
-            System.out.printf("该节点的编号%d已经存在，不能添加\n", heroNode.no);
+            System.out.printf("该结点的编号%d已经存在，不能添加\n", heroNode.no);
         } else {
             heroNode.next = temp.next;
             temp.next = heroNode;
@@ -62,13 +62,13 @@ public class SingleLinkedList {
     }
 
     /**
-     * 根据编号修改节点属性(思路：找到要删除节点的前一个节点的位置，修改next指针域)
+     * 根据编号修改结点属性(思路：找到要删除结点的前一个结点的位置，修改next指针域)
      *
-     * @param newHeroNode 新节点
+     * @param newHeroNode 新结点
      */
     public void updateByNo(HeroNode newHeroNode) {
         HeroNode temp = head;
-        // flag标识是否找到要删除节点的前一个节点所在的位置
+        // flag标识是否找到要删除结点的前一个结点所在的位置
         boolean flag = false;
         while (true) {
             if (temp.next == null) {
@@ -83,12 +83,12 @@ public class SingleLinkedList {
             temp.name = newHeroNode.name;
             temp.nickname = newHeroNode.nickname;
         } else {
-            System.out.printf("编号为%d的节点不存在，无法更新\n", newHeroNode.no);
+            System.out.printf("编号为%d的结点不存在，无法更新\n", newHeroNode.no);
         }
     }
 
     /**
-     * 根据编号来删除节点
+     * 根据编号来删除结点(思路:找到要删除结点的前一个结点位置)
      *
      * @param no
      */
@@ -108,7 +108,7 @@ public class SingleLinkedList {
         if (flag) {
             temp.next = temp.next.next;
         } else {
-            System.out.printf("编号为%d的节点不存在，无法删除\n", no);
+            System.out.printf("编号为%d的结点不存在，无法删除\n", no);
         }
     }
 
@@ -119,6 +119,7 @@ public class SingleLinkedList {
         HeroNode temp = head.next;
         if (temp == null) {
             System.out.println("链表为空");
+            return;
         }
         while (temp != null) {
             System.out.println(temp);
@@ -127,7 +128,7 @@ public class SingleLinkedList {
     }
 
     /**
-     * 获取节点对象的方法
+     * 获取结点对象
      *
      * @param no
      * @param name
@@ -138,6 +139,9 @@ public class SingleLinkedList {
         return new HeroNode(no, name, nickname);
     }
 
+    /**
+     * 单链表结点结构
+     */
     public class HeroNode {
 
         /**
@@ -156,7 +160,7 @@ public class SingleLinkedList {
         private String nickname;
 
         /**
-         * 下个节点指针域
+         * 下个结点指针域
          */
         private HeroNode next;
 
