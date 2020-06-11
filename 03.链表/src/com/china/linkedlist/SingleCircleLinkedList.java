7815cd21a55da078cd9b5d175f923dd7de36b5d7
wuchao <18896820691@163.com>
2020-06-11 23:58:14 +0800

    循环链表的案例实现及测试

--- 03.链表/src/com/china/linkedlist/SingleCircleLinkedList.java
+++ 03.链表/src/com/china/linkedlist/SingleCircleLinkedList.java
@@ -8,7 +8,7 @@ package com.china.linkedlist;
 public class SingleCircleLinkedList {
 
     /**
-     * 第一个结点，不能移动
+     * 第一个数据结点，不能移动
      */
     private Node first;
 
@@ -41,13 +41,51 @@ public class SingleCircleLinkedList {
 
 
     /**
-     * 约瑟夫问题
+     * 约瑟夫问题（小孩报数出圈)
      *
-     * @param startNo 起始编号
      * @param num     结点总数
+     * @param startNo 起始编号
      * @param k       每次报数(1<=k<=num)
      */
-    public void joseph(int startNo, int num, int k) {
+    public void joseph(int num, int startNo, int k) {
+        if (first == null || startNo < 1 || startNo > num) {
+            System.out.println("参数有误,重新输入");
+            return;
+        }
+        // 定义一个辅助结点，先让它指向最后一个结点
+        Node helper = first;
+        while (true) {
+            // 如果helper的next指向第一个结点，表示helper是最后一个结点
+            if (helper.next == first) {
+                break;
+            }
+            // helper后移
+            helper = helper.next;
+        }
+
+        // 将first定位到开始报数的起始结点，并且helper处于first之后
+        for (int i = 0; i < startNo - 1; i++) {
+            first = first.next;
+            helper = helper.next;
+        }
+        // 循环报数并将结点出圈，直到圈内只剩一个结点为止，first指向的始终是要出圈的结点
+        while (true) {
+            // 如果helper与first指向同一个结点，表示只剩一个结点
+            if (helper == first) {
+                break;
+            }
+            // 循环后移
+            for (int j = 0; j < k - 1; j++) {
+                first = first.next;
+                helper = helper.next;
+            }
+            System.out.printf("出圈的结点编号为%d\n", first.no);
+
+            // 将first指向的结点删除
+            first = first.next;
+            helper.next = first;
+        }
+        System.out.printf("最后出圈的结点编号为%d\n", first.no);
     }
 
     /**
--- 03.链表/src/com/china/test/SingleCircleLinkedListTest.java
+++ 03.链表/src/com/china/test/SingleCircleLinkedListTest.java
@@ -10,10 +10,24 @@ import org.junit.Test;
  */
 public class SingleCircleLinkedListTest {
 
+    /**
+     * 测试创建循环链表
+     */
     @Test
     public void testCreate() {
         SingleCircleLinkedList circleLinkedList = new SingleCircleLinkedList();
         circleLinkedList.createNodes(1);
         circleLinkedList.list();
+    }
+
+    /**
+     * 测试约瑟夫问题
+     */
+    @Test
+    public void testJoseph() {
+        SingleCircleLinkedList circleLinkedList = new SingleCircleLinkedList();
+        circleLinkedList.createNodes(5);
+        circleLinkedList.list();
+        circleLinkedList.joseph(5, 1, 2);
     }
 }
